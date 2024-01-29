44ef3043e45d8cc95632dc6dcf5732b55511b68d
Murilo Romeo <muriloh65@gmail.com>
2024-01-29 09:41:58 -0300

    feat: adds bill votes calculation

--- main.rb
+++ main.rb
@@ -7,7 +7,7 @@ class Main
               :votes,
               :vote_results,
               :legislators_votes,
-              :bill_counts
+              :bill_votes
 
   def initialize
     @bills = parse_csv('bills')
@@ -16,11 +16,12 @@ class Main
     @vote_results = parse_csv('vote_results')
 
     @legislators_votes = {}
-    @bill_counts = {}
+    @bill_votes = {}
   end
 
   def call
     calculate_legislators_votes
+    calculate_bill_votes
   end
 
   private
@@ -38,6 +39,28 @@ class Main
 
       legislators_votes[legislator_id]['supported'] += 1 if vote_type == '1'
       legislators_votes[legislator_id]['opposed'] += 1 if vote_type == '2'
+    end
+  end
+
+  def calculate_bill_votes
+    votes.each do |vote|
+      bill_id = vote['bill_id']
+      result = vote_results.find { |result| result['vote_id'] == vote['id'] }
+
+      next if result.nil?
+
+      legislator_id = result['legislator_id']
+      vote_type = result['vote_type']
+
+      bill_votes[bill_id] ||=
+        { 'supporters' => 0, 'opposers' => 0, 'primary_sponsor' => nil }
+
+      bill_votes[bill_id]['supporters'] += 1 if vote_type == '1'
+      bill_votes[bill_id]['opposers'] += 1 if vote_type == '2'
+
+      if legislators.find { |legislator| legislator['id'] == legislator_id }
+        bill_votes[bill_id]['primary_sponsor'] = legislator_id
+      end
     end
   end
 end
